61247a364deb16d909bccf9a18da6e5688e39181
Paolo Reyes <paolo.alfonso.reyes@gmail.com>
2024-10-11 22:09:57 -0600

    feat(joy, twist_mux, drive): Basic controller for driving with twist_mux

---
+++ src/jackal_control_humble/jackal_control_humble/twist_mux_2_cmd_drive.py
@@ -0,0 +1,70 @@
+import rclpy
+
+from rclpy.node import Node 
+from geometry_msgs.msg import Twist
+from clearpath_platform_msgs.msg import Drive
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
+
+class TwistMux2CmdDrive(Node):
+    def __init__(self):
+        super().__init__('twist_mux_2_cmd_drive')
+
+        # Jackal Parameters
+        self.declare_parameter('wheels_length', 0.3765)
+        self.declare_parameter('wheels_radius', 0.095)
+        self.wheels_length = self.get_parameter('wheels_length').get_parameter_value().double_value
+        self.wheels_radius = self.get_parameter('wheels_radius').get_parameter_value().double_value
+
+        # CMD Drive messages
+        self.cmd_drive_msg = Drive()
+        self.cmd_drive_msg.mode = Drive.MODE_VELOCITY
+
+        # Timer
+        self.timer_period = 0.1
+        self.create_timer(self.timer_period, self.timer_callback)
+
+        # Twist Mux Subscription
+        self.create_subscription(
+            Twist,
+            '/jackal_velocity_controller/cmd_vel_unstamped',
+            self.twist_mux_callback,
+            QoSProfile(
+                reliability=QoSReliabilityPolicy.BEST_EFFORT,
+                history=QoSHistoryPolicy.KEEP_LAST,
+                depth=1
+            )
+        )
+
+        # Jackal Publisher
+        self.publisher = self.create_publisher(Drive,
+                                                '/platform/motors/cmd_drive',
+                                                QoSProfile(
+                                                    reliability=QoSReliabilityPolicy.BEST_EFFORT,
+                                                    history=QoSHistoryPolicy.KEEP_LAST,
+                                                    depth=1
+                                                )
+                                            )
+
+    def twist_mux_callback(self, msg):
+        linear_speed = msg.linear.x
+        angular_speed = msg.angular.z
+
+        # Inverse Kinematics
+        left_wheel_speed = (2*linear_speed-self.wheels_length*angular_speed)/(2*self.wheels_radius)
+        right_wheel_speed = (self.wheels_length/self.wheels_radius)*angular_speed+left_wheel_speed
+
+        # Drive Message for Jackal
+        self.cmd_drive_msg.drivers = [left_wheel_speed, right_wheel_speed]
+
+    def timer_callback(self):
+        self.publisher.publish(self.cmd_drive_msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    twist_mux_2_cmd_drive = TwistMux2CmdDrive()
+    rclpy.spin(twist_mux_2_cmd_drive)
+    twist_mux_2_cmd_drive.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ src/jackal_control_humble/launch/jackal_control.launch.py
@@ -0,0 +1,87 @@
+
+import os
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchContext, LaunchDescription
+from launch.substitutions import EnvironmentVariable, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    package_name = 'jackal_control_humble'
+
+    filepath_config_twist_mux = PathJoinSubstitution(
+        [FindPackageShare(package_name), 'config', 'twist_mux.yaml']
+    )
+
+    filepath_config_interactive_markers = PathJoinSubstitution(
+        [FindPackageShare(package_name), 'config', 'teleop_interactive_markers.yaml']
+    )
+
+    filepath_config_jackal = PathJoinSubstitution(
+        [FindPackageShare(package_name), 'config', 'jackal_config.yaml']
+    )
+
+    filepath_config_joy = PathJoinSubstitution(
+        [FindPackageShare(package_name), 'config', ('teleop_ps4.yaml')]
+    )
+
+    node_interactive_marker_twist_server = Node(
+        package='interactive_marker_twist_server',
+        executable='marker_server',
+        name='twist_server_node',
+        remappings={('cmd_vel', 'twist_marker_server/cmd_vel')},
+        parameters=[filepath_config_interactive_markers],
+        output='screen',
+    )
+
+    node_joy = Node(
+        namespace='joy_teleop',
+        package='joy',
+        executable='joy_node',
+        output='screen',
+        name='joy_node',
+        parameters=[filepath_config_joy]
+    )
+
+    node_teleop_twist_joy = Node(
+        namespace='joy_teleop',
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        output='screen',
+        name='teleop_twist_joy_node',
+        parameters=[filepath_config_joy]
+    )
+
+    node_twist_mux = Node(
+        package='twist_mux',
+        executable='twist_mux',
+        output='screen',
+        remappings={('/cmd_vel_out', '/jackal_velocity_controller/cmd_vel_unstamped')},
+        parameters=[filepath_config_twist_mux]
+    )
+
+    node_twist_mux_2_cmd_drive = Node(
+        package='jackal_control_humble',
+        executable='twist_mux_2_cmd_drive',
+        output='screen',
+        name='twist_mux_2_cmd_drive',
+        parameters=[jackal_config]
+    )
+
+    micro_ros_agent = Node(
+        package='micro_ros_agent',
+        executable='micro_ros_agent',
+        output='screen',
+        name='micro_ros_agent',
+        arguments=['serial', '--dev', '/dev/ttyACM0'],
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(node_interactive_marker_twist_server)
+    ld.add_action(node_joy)
+    ld.add_action(node_teleop_twist_joy)
+    ld.add_action(node_twist_mux)
+    ld.add_action(node_twist_mux_2_cmd_drive)
+    ld.add_action(micro_ros_agent)
+    return ld
